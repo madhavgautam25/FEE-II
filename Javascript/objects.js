30aa8e55588ef16c9d95cc6a7c7f9dd2be860572
Madhav Gautam <gautam.madhav63@gmail.com>
2025-08-21 15:49:21 +0530

    Uploaded practice questions for objects

---
+++ Javascript/objects.js
@@ -0,0 +1,39 @@
+// 1. object literals
+// const person = {
+//     name: "Madhav",
+//     age: 19
+// }
+// console.log(person);
+
+
+
+// 2. using new keyword
+// const Person = new Object();
+// Person.name = "Madhav";
+// Person.age = 19;
+// console.log(Person);
+
+
+
+// 3. using constructor
+// function person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+// const p =new  person("Madhav", 19);
+// console.log(p);
+
+
+
+// 4. Using Class
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+// }
+// const p = new Person("Madhav", 19);
+// console.log(p);
+
+
+
