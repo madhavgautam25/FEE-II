dd3091d0879ece31e6e17c989462bcde9988aea2
Madhav Gautam <gautam.madhav63@gmail.com>
2025-08-25 14:40:59 +0530

    Uploaded objects-in-js.js file having 4 questions on objects in javascript

---
+++ Javascript/objects-in-js.js
@@ -0,0 +1,47 @@
+// 1.
+// const laptop = {
+//     brand : "Dell",
+//     price : 55000,
+//     isAvailable : true
+
+// };
+// console.log(laptop);/
+
+
+
+// 2.
+// let employee = { 
+//     name: "John", 
+//     position: "Developer", 
+//     salary: 50000 
+// };
+// employee.salary = 60000;
+// employee.department = "IT";
+// delete employee.position;
+// console.log(employee);
+
+
+
+// 3.
+// let user = {
+//   name: "Anita",
+//   contact: {
+//     email: "anita@example.com",
+//     phone: "9876543210"
+//   }
+// };
+// console.log("Email:", user.contact.email);
+// console.log("Phone:", user.contact.phone);
+
+
+
+// 4.
+// let movie = { 
+//   title: "Inception", 
+//   year: 2010, 
+//   rating: 8.8 
+// };
+// let jsonString = JSON.stringify(movie);
+// console.log("JSON String:", jsonString);
+// let parsedMovie = JSON.parse(jsonString);
+// console.log("Movie Title:", parsedMovie.title);
